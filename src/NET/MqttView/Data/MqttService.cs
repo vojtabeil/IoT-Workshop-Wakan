26b6de2c816e1c12a00f84d7ee28233046955a20
Zly programator <vojtabeil@centrum.cz>
2024-03-24 19:07:35 +0100

    MQTT view web page

---
+++ src/NET/MqttView/Data/MqttService.cs
@@ -0,0 +1,73 @@
+﻿namespace MqttView.Data;
+
+using System.Text;
+
+using MQTTnet;
+using MQTTnet.Client;
+
+public class MqttService : IAsyncDisposable
+{
+    public const int NUMBER_OF_STATIONS = 5;
+
+    public IDictionary<string, string> Clients = new Dictionary<string, string>();
+
+    private IMqttClient? client;
+
+    public event EventHandler? StateUpdated;
+
+    public MqttService()
+    {
+        for (int i = 1; i <= NUMBER_OF_STATIONS; i++)
+        {
+            this.Clients[$"station-{i}"] = string.Empty;
+        }
+    }
+
+    public async Task Initialize()
+    {
+        var mqttFactory = new MqttFactory();
+        var mqttClient = mqttFactory.CreateMqttClient();
+        var mqttClientOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer("192.168.2.1")
+            .Build();
+
+        mqttClient.ApplicationMessageReceivedAsync += async e =>
+            {
+                string data = string.Empty;
+                var bytes = e.ApplicationMessage.PayloadSegment.ToArray();
+
+                try
+                {
+                    data = Encoding.UTF8.GetString(bytes);
+                }
+                catch
+                {
+                    data = "<error>";
+                }
+
+                this.Clients[e.ApplicationMessage.Topic] = data;
+                this.OnStateUpdated();
+                await Task.CompletedTask;
+            };
+
+        await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+        for (int i = 1; i <= NUMBER_OF_STATIONS; i++)
+        {
+            await mqttClient.SubscribeAsync($"station-{i}");
+        }
+
+        this.client = mqttClient;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        this.client?.Dispose();
+        await Task.CompletedTask;
+    }
+
+    protected virtual void OnStateUpdated()
+    {
+        this.StateUpdated?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ src/NET/MqttView/Program.cs
@@ -0,0 +1,29 @@
+using MqttView.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<MqttService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+await app.Services
+    .GetRequiredService<MqttService>()
+    .Initialize();
+
+app.Run();
